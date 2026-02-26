c6bf40dceaf742753662eae8b5061088310b9b7a
Caden <c.sebastian.bartram@gmail.com>
2026-02-25 20:49:41 -0600

    Set up website pages and landing page w explanation Added all skeleton pages and instructions on how to access them, functionality will be implemented soon.

--- index.js
+++ index.js
@@ -5,13 +5,34 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/landingPage.html');
+});
+
+app.get('/buyerHome', (req, res) => {
     res.sendFile(__dirname + '/buyerHome.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/LogIn_page.html');
+});
+
+app.get('/sellerHome', (req, res) => {
+    res.sendFile(__dirname + '/sellerHome.html');
+});
+
+app.get('/adminHome', (req, res) => {
+    res.sendFile(__dirname + '/adminmain.html');
+});
+
+app.get('/adminSub', (req, res) => {
+    res.sendFile(__dirname + '/adminsub.html');
+});
+
+app.get('/adminSub', (req, res) => {
+    res.sendFile(__dirname + '/adminsub.html');
 });
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
 
-app.get('/testHTML', (req, res) => {
-    res.sendFile(__dirname + '/test.html');
-});
