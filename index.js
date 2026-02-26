11746afdd0bad5df0191222da0336a003b4982ba
Caden <c.sebastian.bartram@gmail.com>
2026-02-25 20:02:52 -0600

    re-initialized directory, added test.html to site Following a guide for Express JS, I re-initialized the directory and wrote code to follow along with the tutorial, it was out of my scope of work however, so I rolled back the changes in the code and used the snippet at the end of the guide to add another page to the website.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+
+const app = express();
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/test.html');
-})
+    res.sendFile(__dirname + '/buyerHome.html');
+});
 
-app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
-})
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
+
+app.get('/testHTML', (req, res) => {
+    res.sendFile(__dirname + '/test.html');
+});
