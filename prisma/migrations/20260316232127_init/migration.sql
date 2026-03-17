19eeabe0370470ec2141a833614c24a0c367e508
i-like-burgers <jacquelineclineartist@gmail.com>
2026-03-17 02:32:09 -0500

    forgot to add prisma stuff

--- prisma/migrations/20260316232127_init/migration.sql
+++ prisma/migrations/20260316232127_init/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `role` VARCHAR(191) NOT NULL,
+    `approved` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `User_username_key`(`username`),
